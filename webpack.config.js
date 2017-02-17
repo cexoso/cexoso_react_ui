96d20d89d0899f33c471b896fa599504fe508feb
熊杰 <xiongj01@mySoft.com.cn>
2017-02-17 17:49:59 +0800

    添加webpack-dev-serve

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
 var path = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     /* 输入文件 */
     entry: './src/main.ts',
     output: {
-        /* 输出目录，没有则新建 */
         path: path.resolve(__dirname, './dist'),
-        /* 静态目录，可以直接从这里取文件 */
-        publicPath: '/dist/',
-        /* 文件名 */
         filename: 'build.js'
     },
     module: {
@@ -19,5 +16,6 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
-    }
+    },
+    plugins: [new HtmlWebpackPlugin()]
 }
