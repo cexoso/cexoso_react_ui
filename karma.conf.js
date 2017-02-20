c90a63dd2e4a561f0c2b42907ed0a57f9bff7fda
cexoso <285516193@qq.com>
2017-02-20 22:05:11 +0800

    use phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -73,7 +73,7 @@ module.exports = function (config) {
     },
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
@@ -82,6 +82,9 @@ module.exports = function (config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+    plugin: [
+      "karma-phantomjs-launcher"
+    ]
   })
 }
