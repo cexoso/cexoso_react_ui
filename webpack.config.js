19ce3031a4f9916bb65ffaf9f34ebfd3b49bfc28
cexoso <285516193@qq.com>
2017-02-21 22:55:17 +0800

    需要commander

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,29 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var program = require('commander');
+program
+  .version('0.0.1')
+  .option('-p, --peppers', 'Add peppers')
+  .option('-P, --pineapple', 'Add pineapple')
+  .option('-b, --bbq-sauce', 'Add bbq sauce')
+  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .parse(process.argv);
+
+console.log('you ordered a pizza with:');
+if (program.peppers) console.log('  - peppers');
+if (program.pineapple) console.log('  - pineapple');
+if (program.bbqSauce) console.log('  - bbq');
+console.log('  - %s cheese', program.cheese);
 module.exports = {
-    entry: './example/main.tsx',
+    entry: {
+        index: './example/main.tsx',
+        vendor: ['react', 'react-dom',"lodash"]
+    },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'build.js'
+        filename:  "[name].[chunkHash:8].js",
+        chunkFilename: "[name].[chunkHash:8].js",
     },
     module: {
         rules: [
@@ -22,6 +40,8 @@ module.exports = {
     devtool: 'source-map',
     plugins: [
         new HtmlWebpackPlugin({template: "./index.html"}),
-        new webpack.LoaderOptionsPlugin({debug: true})
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor','manifest']
+        }),
     ]
 }
